db26b36e673ae0f6337e3a89a357e0940366cc69
Chris Schweda <cschweda@gmail.com>
2018-02-07 07:47:26 -0600

    Add floating nav

--- src/pages/Article.vue
+++ src/pages/Article.vue
@@ -2,36 +2,27 @@
 
     <section class="page-wrapper">
 
-        <div :class="{ ' open ': drawer}" class="overlay">
-            <div class="outer-close toggle-overlay">
-                <a class="close">
-                    <span></span>
-                </a>
-            </div>
-            <nav>
+
+
+        <header>
+            <div class="nav nav-float nav-float--unpinned">
                 <ul>
-                    <li>
-                        <a @click.prevent>About Us</a>
-                    </li>
-                    <li>
-                        <a @click.prevent>Contact</a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/ICJIA/icjia-tableau">GitHub Source</a>
+                    <li class="home">
+                        <a href="#">ICJIA Home</a>
                     </li>
 
+                    <li class="about">
+                        <a href="#">About</a>
+                    </li>
+                    <li class="news">
+                        <a href="#">Newsletter</a>
+                    </li>
+                    <li class="contact">
+                        <a href="#">Contact</a>
+                    </li>
                 </ul>
-            </nav>
-        </div>
-
-
-        <div class="burger">
-            <button class="hamburger hamburger--collapse" :class="{ ' is-active ': drawer}" type="button" @click.stop="toggleDrawer">
-                <span class="hamburger-box">
-                    <span class="hamburger-inner hamburger-overlay"></span>
-                </span>
-            </button>
-        </div>
+            </div>
+        </header>
 
         <hero />
 
@@ -76,6 +67,7 @@
     import Viz from "@/components/Viz"
     import { sample01, sample02 } from "@/code";
     const uuidv4 = require('uuid/v4');
+    import _ from 'lodash';
 
     if (!('remove' in Element.prototype)) {
         Element.prototype.remove = function () {
@@ -89,7 +81,7 @@
         name: "A0",
         mounted: function () {
             var scrollObject = {};
-            window.onscroll = getScrollPosition;
+            window.onscroll = _.debounce(getScrollPosition);
 
             function getScrollPosition() {
                 scrollObject = {
@@ -97,17 +89,16 @@
                     y: window.pageYOffset
                 }
 
-                if (scrollObject.y > 740) {
-                    document.getElementsByClassName("hamburger-inner")[0].classList.add('hamburger-dark')
-                    document.getElementsByClassName("hamburger-inner")[0].classList.remove('hamburger-overlay')
-                    document.getElementsByClassName("hamburger-inner")[0].classList.remove('hamburger-light')
+                if (scrollObject.y > 730) {
+                    document.getElementsByClassName("nav-float")[0].classList.add('nav-float--pinned')
+                    document.getElementsByClassName("nav-float")[0].classList.remove('nav-float--unpinned')
 
-                    //console.log(document.getElementsByClassName("burger")[0].classList)
+
 
                 } else {
-                    document.getElementsByClassName("hamburger-inner")[0].classList.add('hamburger-light')
-                    document.getElementsByClassName("hamburger-inner")[0].classList.add('hamburger-overlay')
-                    document.getElementsByClassName("hamburger-inner")[0].classList.remove('hamburger-dark')
+                    document.getElementsByClassName("nav-float")[0].classList.remove('nav-float--pinned')
+                    document.getElementsByClassName("nav-float")[0].classList.add('nav-float--unpinned')
+
                 }
             }
 
@@ -116,22 +107,6 @@
             generateUUID: function () {
                 return "" + uuidv4();
             },
-            toggleDrawer: function (e) {
-                let el = document.getElementsByClassName("hamburger-inner")[0].classList
-                if (!el.contains('hamburger-overlay')) {
-                    document.getElementsByClassName("hamburger-inner")[0].classList.add('hamburger-overlay')
-                }
-                this.drawer = !this.drawer;
-                this.drawer
-                    ? document.body.classList.add("no-scroll") :
-                    document.body.classList.remove("no-scroll")
-                this.drawer
-                    ? document.getElementsByClassName("hamburger-inner")[0].classList.add('hamburger-overlay') : document.getElementsByClassName("hamburger-inner")[0].classList.remove('hamburger-overlay')
-                this.drawer
-                    ? (document.getElementsByClassName("overlay")[0].style.height = "100%")
-                    : (document.getElementsByClassName("overlay")[0].style.height = "0%");
-                return;
-            }
 
 
         },
@@ -237,146 +212,99 @@
         background: red;
     }
 
-    .topnav {
-        overflow: hidden;
-        background-color: #fff;
+
+
+    /** New nav
+     * 
+     */
+
+    .nav {
+        position: fixed;
+        /* Set the navbar to fixed position */
         top: 0;
-        z-index: 100;
-        position: fixed;
+        /* Position the navbar at the top of the page */
         width: 100%;
-        border-bottom: 1px solid #ccc;
-        -webkit-box-shadow: 2px 5px 8px -2px rgba(156, 154, 156, 0.47);
-        -moz-box-shadow: 2px 5px 8px -2px rgba(156, 154, 156, 0.47);
-        box-shadow: 2px 5px 8px -2px rgba(156, 154, 156, 0.47);
-        display: flex;
+        /* Full width */
+        z-index: 1000;
     }
 
-    .burger {
-
-        width: 50px;
-        vertical-align: middle;
-        position: fixed;
-        top: 10px;
-        left: 10px;
-        z-index: 20;
-    }
-
-
-
-    .burger,
-    .burger *,
-    button {
-        outline: none !important;
-    }
-
-    .hamburger-inner,
-    .hamburger-inner::before,
-    .hamburger-inner::after {
-        width: 30px;
-        height: 4px;
-        background-color: #555;
-        border-radius: 4px;
-        position: absolute;
-    }
-
-    .hamburger-inner.hamburger-light,
-    .hamburger-inner.hamburger-light::before,
-    .hamburger-inner.hamburger-light::after {
-
-        background-color: #fff;
-
-    }
-
-    .hamburger-inner.hamburger-dark,
-    .hamburger-inner.hamburger-dark::before,
-    .hamburger-inner.hamburger-dark::after {
-
-        background-color: #555;
-
-    }
-
-    .hamburger-inner.hamburger-overlay,
-    .hamburger-inner.hamburger-overlay::before,
-    .hamburger-inner.hamburger-overlay::after {
-
-        background-color: #fff;
-
-    }
-
-    /* The Overlay (background) */
-
-    .overlay {
-        position: fixed;
-        width: 100%;
-        height: 0%;
-        top: 0;
-        left: 0;
-        background: linear-gradient( 200deg,
-        rgb(23, 23, 24),
-        rgb(42, 42, 43),
-        rgb(110, 109, 110));
-        opacity: 0;
-        visibility: hidden;
-        -webkit-transition: all 0.5s ease;
-        transition: all 0.5s ease;
-        z-index: 10;
-    }
-
-    .open {
-        opacity: 0.98;
-        visibility: visible;
-    }
-
-    nav {
-        text-align: center;
-        height: 100vh;
-        display: -webkit-box;
-        display: -ms-flexbox;
-        display: flex;
-        -webkit-box-orient: vertical;
-        -webkit-box-direction: normal;
-        -ms-flex-direction: column;
-        flex-direction: column;
-        -webkit-box-pack: center;
-        -ms-flex-pack: center;
-        justify-content: center;
-    }
-
-    nav ul {
-        margin: 0;
-        padding: 0;
+    .nav ul {
         list-style: none;
+        background-color: #555;
+        text-align: center;
+        padding: 0;
+        margin: 0;
     }
 
-    nav ul li {
-        -webkit-box-align: center;
-        -ms-flex-align: center;
-        align-items: center;
-        -webkit-box-flex: 1;
-        -ms-flex: 1;
-        flex: 1;
-        line-height: 8vh;
+    .nav li {
+        font-family: 'Oswald', sans-serif;
+        font-size: 1.2em;
+        line-height: 40px;
+        height: 40px;
+        border-bottom: 1px solid #888;
     }
 
-    nav ul li a {
-        font-size: 2em;
-        font-weight: 900;
-        -webkit-transition: all 0.5s ease;
-        transition: all 0.5s ease;
-        display: block;
+    .nav a {
         text-decoration: none;
         color: #fff;
-        opacity: 1;
-        display: inline-block;
-        cursor: pointer;
+        display: block;
+        transition: .3s background-color;
     }
 
-    nav ul li a:hover {
-        color: rgb(245, 1, 1);
-        -webkit-transform: scale(1.1);
-        transform: scale(1.1);
+    .nav a:hover {
+        background-color: orange;
     }
 
+    .nav a.active {
+        background-color: #fff;
+        color: #444;
+        cursor: default;
+    }
+
+    @media screen and (min-width: 600px) {
+        .nav li {
+            width: 120px;
+            border-bottom: none;
+            height: 50px;
+            line-height: 50px;
+            font-size: 1.4em;
+        }
+
+        /* Option 1 - Display Inline */
+        .nav li {
+            display: inline-block;
+            margin-right: -4px;
+        }
+
+        /* Options 2 - Float
+  .nav li {
+    float: left;
+  }
+  .nav ul {
+    overflow: auto;
+    width: 600px;
+    margin: 0 auto;
+  }
+  .nav {
+    background-color: #444;
+  }
+  */
+    }
+
+    .nav-float {
+        will-change: transform;
+        transition: transform 500ms linear;
+    }
+
+    .nav-float--pinned {
+
+        transform: translateY(0%);
+    }
+
+    .nav-float--unpinned {
+
+        transform: translateY(-200%);
+    }
 
 
 
